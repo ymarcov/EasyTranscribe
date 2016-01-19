7b536675f38adec120609e6852ae9bdf64bb2815
Yam Marcovic <ymarcov@gmail.com>
2016-01-19 14:39:15 +0200

    Move private UI stuff to UI::Detail

--- ui.rb
+++ ui.rb
@@ -4,225 +4,222 @@ require 'gtk3'
 
 module EasyTranscribe
   module UI
-    private
+    module Detail
+      @on_exit = nil
 
-    @on_exit = nil
+      def self.create_window
+        win = Gtk::Window.new
+        win.title = 'EasyTranscribe'
+        win.set_border_width(20)
 
-    def self.create_window
-      win = Gtk::Window.new
-      win.title = 'EasyTranscribe'
-      win.set_border_width(20)
-
-      win.signal_connect('delete_event') do
-        @on_exit.call if @on_exit
-        Gtk.main_quit
-        false
-      end
-
-      win
-    end
-
-    def self.create_footer_items
-      {
-        save: Gtk::Button.new(label: 'Save'),
-        export: Gtk::Button.new(label: 'Export')
-      }
-    end
-
-    def self.create_footer
-      box = Gtk::Box.new(:horizontal)
-
-      box.instance_eval do
-        class << self
-          attr_reader :buttons
+        win.signal_connect('delete_event') do
+          @on_exit.call if @on_exit
+          Gtk.main_quit
+          false
         end
 
-        @buttons = UI.create_footer_items
+        win
       end
 
-      box.buttons.each do |_, i|
-        i.set_border_width(5)
-        box.pack_start(i)
+      def self.create_footer_items
+        {
+          save: Gtk::Button.new(label: 'Save'),
+          export: Gtk::Button.new(label: 'Export')
+        }
       end
 
-      box
-    end
+      def self.create_footer
+        box = Gtk::Box.new(:horizontal)
 
-    def self.create_toolbar_items
-      buttons = {
-        open: Gtk::Button.new(label: 'Open'),
-        _sep_0: Gtk::SeparatorToolItem.new,
-        stop: Gtk::Button.new(label: 'Stop'),
-        play: Gtk::Button.new(label: 'Play/Pause'),
-        _sep_1: Gtk::SeparatorToolItem.new,
-        rewind: Gtk::Button.new(label: 'Rewind'),
-        fast_forward: Gtk::Button.new(label: 'Fast Forward'),
-        _sep_2: Gtk::SeparatorToolItem.new,
-        _lbl_2: Gtk::Label.new('Segment: '),
-        start_segment: Gtk::Button.new(label: 'Start'),
-        end_segment: Gtk::Button.new(label: 'End'),
-        clear_end_segment: Gtk::Button.new(label: 'Clear End'),
-        restart_segment: Gtk::Button.new(label: 'Restart'),
-      }
+        box.instance_eval do
+          class << self
+            attr_reader :buttons
+          end
 
-      buttons[:open].set_tooltip_markup('<b>Ctrl+O</b> Open an audio file')
-      buttons[:stop].set_tooltip_markup('<b>Ctrl+W</b> Stop and rewind track to beginning')
-      buttons[:play].set_tooltip_markup('<b>Ctrl+Space</b> Play or Pause track')
-      buttons[:rewind].set_tooltip_markup('<b>Ctrl+R</b> Rewind track')
-      buttons[:fast_forward].set_tooltip_markup('<b>Ctrl+F</b> Fast-forward track')
-      buttons[:start_segment].set_tooltip_markup('<b>Ctrl+1</b> Set segment start at cursor position')
-      buttons[:end_segment].set_tooltip_markup('<b>Ctrl+2</b> Set segment end at cursor position')
-      buttons[:clear_end_segment].set_tooltip_markup('<b>Ctrl+0</b> Clear end segment')
-      buttons[:restart_segment].set_tooltip_markup('<b>Ctrl+E</b> Restart segment')
-
-      buttons
-    end
-
-    def self.create_toolbar_accelerators(buttons)
-      group = Gtk::AccelGroup.new
-
-      {
-        open: '<Control>o',
-        stop: '<Control>w',
-        play: '<Control>space',
-        rewind: '<Control>r',
-        fast_forward: '<Control>f',
-        start_segment: '<Control>1',
-        end_segment: '<Control>1',
-        clear_end_segment: '<Control>0',
-        restart_segment: '<Control>e',
-      }.each do |id, accel|
-        key, mod = Gtk.accelerator_parse(accel)
-        buttons[id].add_accelerator('clicked', group, key, mod, Gtk::AccelFlags::VISIBLE)
-      end
-
-      group
-    end
-
-    def self.create_toolbar
-      tb = Gtk::Box.new(:vertical)
-
-      button_box = Gtk::Box.new(:horizontal)
-
-      tb.instance_eval do
-        @items = UI.create_toolbar_items.freeze
-        @buttons = @items.select { |_, w| w.kind_of?(Gtk::Button) }.freeze
-      end
-
-      def tb.buttons
-        @buttons
-      end
-
-      def tb.items
-        @items
-      end
-
-      tb.items.each do |_, b|
-        b.set_border_width(2) if b.respond_to?(:set_border_width)
-        button_box.pack_start(b)
-      end
-
-      def tb.slider
-        unless defined? @scale
-          @scale = Gtk::Scale.new(:horizontal)
+          @buttons = Detail.create_footer_items
         end
 
-        @scale
-      end
-
-      tb.pack_start(tb.slider, padding: 5)
-      tb.pack_start(button_box, padding: 10)
-
-      tb
-    end
-
-    def self.create_textview
-      frame = Gtk::Frame.new
-
-      def frame.editor
-        unless defined? @tv
-          win = Gtk::ScrolledWindow.new
-          @tv = Gtk::TextView.new
-          @tv.set_left_margin(10)
-          @tv.set_indent(20)
-          @tv.set_wrap_mode(Gtk::WrapMode::WORD_CHAR)
-          @tv.set_pixels_above_lines(10)
-          @tv.show
-          win.add(@tv)
-          win.show
-          add(win)
+        box.buttons.each do |_, i|
+          i.set_border_width(5)
+          box.pack_start(i)
         end
 
-        @tv
+        box
       end
 
-      frame
-    end
+      def self.create_toolbar_items
+        buttons = {
+          open: Gtk::Button.new(label: 'Open'),
+          _sep_0: Gtk::SeparatorToolItem.new,
+          stop: Gtk::Button.new(label: 'Stop'),
+          play: Gtk::Button.new(label: 'Play/Pause'),
+          _sep_1: Gtk::SeparatorToolItem.new,
+          rewind: Gtk::Button.new(label: 'Rewind'),
+          fast_forward: Gtk::Button.new(label: 'Fast Forward'),
+          _sep_2: Gtk::SeparatorToolItem.new,
+          _lbl_2: Gtk::Label.new('Segment: '),
+          start_segment: Gtk::Button.new(label: 'Start'),
+          end_segment: Gtk::Button.new(label: 'End'),
+          clear_end_segment: Gtk::Button.new(label: 'Clear End'),
+          restart_segment: Gtk::Button.new(label: 'Restart'),
+        }
 
-    def self.add_css
-      css = '
-      GtkTextView {
-        font-size: 12px;
-        font-family: "DroidSerif", "Georgia", serif;
-      }
-      '
+        buttons[:open].set_tooltip_markup('<b>Ctrl+O</b> Open an audio file')
+        buttons[:stop].set_tooltip_markup('<b>Ctrl+W</b> Stop and rewind track to beginning')
+        buttons[:play].set_tooltip_markup('<b>Ctrl+Space</b> Play or Pause track')
+        buttons[:rewind].set_tooltip_markup('<b>Ctrl+R</b> Rewind track')
+        buttons[:fast_forward].set_tooltip_markup('<b>Ctrl+F</b> Fast-forward track')
+        buttons[:start_segment].set_tooltip_markup('<b>Ctrl+1</b> Set segment start at cursor position')
+        buttons[:end_segment].set_tooltip_markup('<b>Ctrl+2</b> Set segment end at cursor position')
+        buttons[:clear_end_segment].set_tooltip_markup('<b>Ctrl+0</b> Clear end segment')
+        buttons[:restart_segment].set_tooltip_markup('<b>Ctrl+E</b> Restart segment')
 
-      style_provider = Gtk::CssProvider.new
-      style_provider.load_from_data(css)
+        buttons
+      end
 
-      Gtk::StyleContext.add_provider_for_screen(
-        Gdk::Screen.default,
-        style_provider,
-        Gtk::StyleProvider::PRIORITY_APPLICATION
-      )
-    end
+      def self.create_toolbar_accelerators(buttons)
+        group = Gtk::AccelGroup.new
 
-    def self.create_main_window_widgets
-      vbox = Gtk::Box.new(:vertical)
+        {
+          open: '<Control>o',
+          stop: '<Control>w',
+          play: '<Control>space',
+          rewind: '<Control>r',
+          fast_forward: '<Control>f',
+          start_segment: '<Control>1',
+          end_segment: '<Control>1',
+          clear_end_segment: '<Control>0',
+          restart_segment: '<Control>e',
+        }.each do |id, accel|
+          key, mod = Gtk.accelerator_parse(accel)
+          buttons[id].add_accelerator('clicked', group, key, mod, Gtk::AccelFlags::VISIBLE)
+        end
 
-      textview = create_textview
-      toolbar = create_toolbar
-      footer = create_footer
+        group
+      end
 
-      vbox.pack_start(toolbar, expand: false, fill: false)
-      vbox.pack_start(textview, expand: true, fill: true)
-      vbox.pack_start(footer, expand: false, fill: false)
+      def self.create_toolbar
+        tb = Gtk::Box.new(:vertical)
 
-      return {
-        layout: vbox,
-        toolbar: toolbar,
-        textview: textview,
-        footer: footer
-      }
-    end
+        button_box = Gtk::Box.new(:horizontal)
 
-    def self.bind_commands(buttons, command_source)
-      buttons.each do |name, widget|
-        if widget.kind_of?(Gtk::Button)
-          widget.signal_connect('clicked') do
-            if command_source.respond_to?(name)
-              command_source.method(name).call
+        tb.instance_eval do
+          @items = Detail.create_toolbar_items.freeze
+          @buttons = @items.select { |_, w| w.kind_of?(Gtk::Button) }.freeze
+        end
+
+        def tb.buttons
+          @buttons
+        end
+
+        def tb.items
+          @items
+        end
+
+        tb.items.each do |_, b|
+          b.set_border_width(2) if b.respond_to?(:set_border_width)
+          button_box.pack_start(b)
+        end
+
+        def tb.slider
+          unless defined? @scale
+            @scale = Gtk::Scale.new(:horizontal)
+          end
+
+          @scale
+        end
+
+        tb.pack_start(tb.slider, padding: 5)
+        tb.pack_start(button_box, padding: 10)
+
+        tb
+      end
+
+      def self.create_textview
+        frame = Gtk::Frame.new
+
+        def frame.editor
+          unless defined? @tv
+            win = Gtk::ScrolledWindow.new
+            @tv = Gtk::TextView.new
+            @tv.set_left_margin(10)
+            @tv.set_indent(20)
+            @tv.set_wrap_mode(Gtk::WrapMode::WORD_CHAR)
+            @tv.set_pixels_above_lines(10)
+            @tv.show
+            win.add(@tv)
+            win.show
+            add(win)
+          end
+
+          @tv
+        end
+
+        frame
+      end
+
+      def self.add_css
+        css = '
+        GtkTextView {
+          font-size: 12px;
+          font-family: "DroidSerif", "Georgia", serif;
+        }
+        '
+
+        style_provider = Gtk::CssProvider.new
+        style_provider.load_from_data(css)
+
+        Gtk::StyleContext.add_provider_for_screen(
+          Gdk::Screen.default,
+          style_provider,
+          Gtk::StyleProvider::PRIORITY_APPLICATION
+        )
+      end
+
+      def self.create_main_window_widgets
+        vbox = Gtk::Box.new(:vertical)
+
+        textview = create_textview
+        toolbar = create_toolbar
+        footer = create_footer
+
+        vbox.pack_start(toolbar, expand: false, fill: false)
+        vbox.pack_start(textview, expand: true, fill: true)
+        vbox.pack_start(footer, expand: false, fill: false)
+
+        return {
+          layout: vbox,
+          toolbar: toolbar,
+          textview: textview,
+          footer: footer
+        }
+      end
+
+      def self.bind_commands(buttons, command_source)
+        buttons.each do |name, widget|
+          if widget.kind_of?(Gtk::Button)
+            widget.signal_connect('clicked') do
+              if command_source.respond_to?(name)
+                command_source.method(name).call
+              end
             end
           end
         end
       end
     end
 
-
-    public
-
     def self.setup(opts = {})
-      add_css
+      Detail.add_css
 
-      widgets = create_main_window_widgets
+      widgets = Detail.create_main_window_widgets
 
-      win = create_window
+      win = Detail.create_window
       win.add(widgets[:layout])
-      win.add_accel_group(create_toolbar_accelerators(widgets[:toolbar].buttons))
+      win.add_accel_group(Detail.create_toolbar_accelerators(widgets[:toolbar].buttons))
       win.show_all
 
-      bind_commands(widgets[:toolbar].buttons, opts[:commands])
-      bind_commands(widgets[:footer].buttons, opts[:commands])
+      Detail.bind_commands(widgets[:toolbar].buttons, opts[:commands])
+      Detail.bind_commands(widgets[:footer].buttons, opts[:commands])
       win.set_focus(widgets[:textview].editor)
 
       @main_window = win
@@ -253,8 +250,6 @@ module EasyTranscribe
     end
 
     module Slider
-      private
-
       def self.init(scale)
         @scale = scale
         @scale.set_range(0, 1)
@@ -263,8 +258,6 @@ module EasyTranscribe
         @length = nil
         reset
       end
-
-      public
 
       def self.reset(length = nil)
         @scale.clear_marks
